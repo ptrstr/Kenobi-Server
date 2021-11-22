824446be93a88614ec7b38d58727b7062cf0dda6
Aayush9029 <aayushpokharel36@gmail.com>
2021-11-21 19:21:37 -0500

    pep8 for folder

---
+++ kenobi_server/tests/test_parser.py
@@ -0,0 +1,15 @@
+"""
+Test file for the project.
+"""
+
+from kenobi_server.src.message_parser import MessageParser
+
+
+def test_parse_message_valid_message():
+    """
+    Test that a valid message is parsed correctly.
+    """
+    message = "Hello, World!"
+    parser = MessageParser()
+    parsed_message = parser.parse(message)
+    assert parsed_message == "Hello, World!"
